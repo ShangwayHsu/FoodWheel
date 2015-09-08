9f2a6b96076d35eb090ebc802c037abb2a8793a3
Vincent Yang <VincentYang2014@gmail.com>
2015-09-07 18:57:36 -0700

    Singleton FoodWheel base class

---
+++ src/sample/FoodWheel.java
@@ -0,0 +1,27 @@
+package sample;
+
+/**
+ * Created by VincentYang on 9/7/15.
+ * This is a singleton design pattern
+ */
+public class FoodWheel {
+   //FIELDS
+    private FoodWheel instance;
+    
+   //CONSTRUCTOR
+    private FoodWheel()
+    {
+
+    }
+    //METHODS
+    public FoodWheel getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new FoodWheel();
+        }
+        return instance;
+
+    }
+
+}
