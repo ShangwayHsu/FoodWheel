a414ae3b879f4b9e72f7658e266fa2b9f9e67473
Vincent Yang <VincentYang2014@gmail.com>
2015-09-07 19:03:13 -0700

    Base Vendor and VendorFactory Classes

---
+++ src/sample/Vendor.java
@@ -0,0 +1,9 @@
+package sample;
+
+/**
+ * Created by VincentYang on 9/7/15.
+ * This is a factory made item
+ */
+public class Vendor {
+
+}
---
+++ src/sample/VendorFactory.java
@@ -0,0 +1,13 @@
+package sample;
+
+/**
+ * Created by VincentYang on 9/7/15.
+ * This class creates vendors
+ */
+public class VendorFactory {
+    //FIELDS
+
+    //CONSTRUCTORS
+    
+    //METHODS
+}
